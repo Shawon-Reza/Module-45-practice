a21333caa7bd66ae282b29d7b898ec13fca97ec2
Shawon Reza <shawon.reza.sr81@gmail.com>
2024-11-19 20:58:42 +0600

    USe NavLink

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,4 +1,5 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, NavLink, Outlet, useLocation } from "react-router-dom";
+import './Home.css'
 
 const Home = () => {
     const location=useLocation()
@@ -6,10 +7,12 @@ const Home = () => {
     return (
         <div className="border border-4 border-lime-600 rounded-xl p-3">
             <div className="flex justify-around">
-                <Link to="/contact" className="font-extrabold p-1
-                bg-gray-300 rounded-lg hover:bg-orange-500 hover:text-white">Contact</Link>
-                <Link to="/" className="font-extrabold p-1
-                bg-gray-300 rounded-lg hover:bg-orange-500 hover:text-white">Home</Link>
+                <NavLink to="/contact" className="font-extrabold p-1
+                bg-gray-300 rounded-lg hover:bg-orange-500 hover:text-white">Contact</NavLink>
+
+                <NavLink to="/" className="font-extrabold p-1
+                bg-gray-300 rounded-lg hover:bg-orange-500 hover:text-white">Home</NavLink>
+
             </div>
             <h1 className="mt-3">Home page : Practicing React Ruting</h1>
 
